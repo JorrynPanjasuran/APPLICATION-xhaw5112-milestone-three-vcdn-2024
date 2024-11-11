fafb0ef84bb39924639c49170a4f69a66e352bf5
JorrynPanjasuran <163964500+JorrynPanjasuran@users.noreply.github.com>
2024-11-11 02:06:29 +0200

    CODE ATTRIBUTION

--- screens/SewingScreen.tsx
+++ screens/SewingScreen.tsx
@@ -1,3 +1,55 @@
+// CODE ATTRIBUTION
+// TITLE: React Native. (2024) Navigating Between Screens
+// AVAILABLE: https://reactnative.dev/docs/navigation (Accessed: 1 November 2024)
+
+// CODE ATTRIBUTION
+// TITLE: React Navigation. (2024) Drawer Navigation
+// AVAILABLE: https://reactnavigation.org/docs/drawer-based-navigation/ (Accessed: 1 November 2024)
+
+// CODE ATTRIBUTION
+// TITLE: React Native. (2024) Navigating Between Screens
+// AVAILABLE: https://reactnative.dev/docs/navigation (Accessed: 11 November 2024)
+
+// CODE ATTRIBUTION
+// TITLE: React Navigation. (2024) Drawer Navigation
+// AVAILABLE: https://reactnavigation.org/docs/drawer-based-navigation/ (Accessed: 11 November 2024)
+
+
+// CODE ATTRIBUTION
+// TITLE: IIE Module Manual 2024
+// AUTHOR: The Independent Institute of Education (Pty) Ltd
+// DATE: 09/02/2023
+// VERSION: First Edition: 2012
+// AVAILABLE: https://advtechonline.sharepoint.com/sites/TertiaryStudents/IIE%20Student%20Materials/Forms/Default%20View.aspx?id=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024%2FWEDE5020MM%20%2D%20REVISED%209%20Feb%202023%2Epdf&viewid=db15e059%2D4f93%2D487f%2Dabda%2De538b821c7b8&parent=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024
+
+// CODE ATTRIBUTION
+// TITLE: XHAW5112/d/p/w WORK INTEGRATED LEARNING 1 MODULE MANUAL
+// AUTHOR: The Independent Institute of Education (Pty) Ltd
+// DATE: 09/02/2023
+// VERSION: 2024 Edition
+// AVAILABLE: https://advtechonline.sharepoint.com/sites/TertiaryStudents/IIE%20Student%20Materials/Forms/Default%20View.aspx?id=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024%2FWEDE5020MM%20%2D%20REVISED%209%20Feb%202023%2Epdf&viewid=db15e059%2D4f93%2D487f%2Dabda%2De538b821c7b8&parent=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024
+
+// CODE ATTRIBUTION
+// TITLE: Principles of UI and UX Design - PIXD5112d/p/w MODULE OUTLINE
+// AUTHOR: The Independent Institute of Education (Pty) Ltd
+// DATE: 09/02/2023
+// VERSION: 2024 Edition (First published in 2022)
+// AVAILABLE:https://advtechonline.sharepoint.com/sites/TertiaryStudents/IIE%20Student%20Materials/Forms/Default%20View.aspx?id=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024%2FWEDE5020MM%20%2D%20REVISED%209%20Feb%202023%2Epdf&viewid=db15e059%2D4f93%2D487f%2Dabda%2De538b821c7b8&parent=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024
+
+// CODE ATTRIBUTION
+// TITLE: WEB DEVELOPMENT - WEDE5020/d/p/w MODULE MANUAL/GUIDE
+// AUTHOR: The Independent Institute of Education (Pty) Ltd
+// DATE: 09/02/2023
+// VERSION: 2024 Edition (First published in 2012)
+// AVAILABLE: https://advtechonline.sharepoint.com/sites/TertiaryStudents/IIE%20Student%20Materials/Forms/Default%20View.aspx?id=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024%2FWEDE5020MM%20%2D%20REVISED%209%20Feb%202023%2Epdf&viewid=db15e059%2D4f93%2D487f%2Dabda%2De538b821c7b8&parent=%2Fsites%2FTertiaryStudents%2FIIE%20Student%20Materials%2FNew%20Student%20Materials%20CAT%2FWEDE5020%2F2024
+
+// CODE ATTRIBUTION
+// TITLE: HTML Tutorial
+// AUTHOR: W3Schools
+// DATE: 09/02/2023
+// VERSION: N/A
+// AVAILABLE: https://www.w3schools.com/html/ [Accessed 8 Nov. 2024]
+
 // SewingScreen.tsx
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
